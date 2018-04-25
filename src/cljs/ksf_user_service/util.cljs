1d23e39273ed7f0bcfebe85276b9e2a054a4db03
Valtteri Pajunen <valtteri.pajunen@gmail.com>
2018-04-25 21:39:53 +0300

    remove wrapper fn

--- src/cljs/ksf_user_service/util.cljs
+++ src/cljs/ksf_user_service/util.cljs
@@ -1,12 +1,11 @@
 (ns ksf-user-service.util)
 
 (defn input [id type placeholder value]
- (fn []
-   [:input {:id id
-            :type type
-            :placeholder placeholder
-            :class "form-control"
-            :on-change #(reset! value (-> % .-target .-value))}]))
+  [:input {:id id
+          :type type
+          :placeholder placeholder
+          :class "form-control"
+          :on-change #(reset! value (-> % .-target .-value))}])
 
 (defn page-header [header-text]
   [:div.row {:class "page-header"}
